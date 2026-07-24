095ec37795bd07d725a9705a192eedfcc6363017
Harsh Wakade <harshadwakade1234@gmail.com>
2026-07-24 17:42:26 +0530

    Update actions.ts

--- app/admin/actions.ts
+++ app/admin/actions.ts
@@ -1,37 +1,29 @@
 "use server";
 
-import { db } from "@/db";
-import { foodsTable } from "@/db/schema";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export async function login(formData: FormData): Promise<void> {
+  const password = formData.get("password")?.toString();
 
-export async function addFood(formData: FormData) {
-    const cookieStore = await cookies();
-
-  if (cookieStore.get("admin")?.value !== "true") {
-    redirect("/admin/login");
+  // Debug check
+  if (!process.env.ADMIN_PASSWORD) {
+    throw new Error("ADMIN_PASSWORD is missing");
   }
-  const name = formData.get("name") as string;
-  const category = formData.get("category") as string;
-  const description = formData.get("description") as string;
-  const ingredients = formData.get("ingredients") as string;
-  const image = formData.get("image") as string;
-  const price = Number(formData.get("price"));
-  const rating = Number(formData.get("rating"));
 
-  const slug = name
-    .toLowerCase()
-    .replace(/\s+/g, "-");
+  if (password !== process.env.ADMIN_PASSWORD) {
+    redirect("/admin/login?error=1");
+  }
 
-  await db.insert(foodsTable).values({
-    name,
-    category,
-    description,
-    ingredients,
-    image,
-    price,
-    rating,
-    slug,
+  const cookieStore = await cookies();
+
+  cookieStore.set("admin", "true", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 2, // 2 hours
   });
+
+  redirect("/admin");
 }
