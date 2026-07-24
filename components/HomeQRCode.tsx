5c59a5ed1b8b21b5cfa39761ad25348efda697a7
Harsh Wakade <harshadwakade1234@gmail.com>
2026-07-24 17:03:39 +0530

    Update HomeQRCode.tsx

--- components/HomeQRCode.tsx
+++ components/HomeQRCode.tsx
@@ -10,7 +10,7 @@ export default function HomeQRCode() {
     const router = useRouter();
 
     useEffect(() => {
-        QRCode.toDataURL("https://scanby-buy.vercel.app/home", {
+        QRCode.toDataURL("scanby-buy.vercel.app/home", {
             width: 350,
             margin: 2,
             color: {
@@ -22,7 +22,7 @@ export default function HomeQRCode() {
 
     return (
         <div
-            onClick={() => router.push("https://scanby-buy.vercel.app/home")}
+            onClick={() => router.push("scanby-buy.vercel.app/home")}
             className="cursor-pointer rounded-2xl bg-white p-2 transition hover:scale-105"
         >
             {qr && (
